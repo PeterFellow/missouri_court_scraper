b5f5659cb37d6d454938e73739ffada7a2af5c11
Kaleb Miller <kalebmiller@Kalebs-MacBook-Pro-7.local>
2025-07-10 13:02:08 -0500

    Added original, legacy, and appeals case numbers to be checked.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 case_number = input("Enter your case number: ")
 
-def is_valid_case_number(case_number):
+def is_modern_format(case_number):
     if len(case_number) != 12:
         return False
 
@@ -26,7 +26,57 @@ def is_valid_case_number(case_number):
 
     return True
 
-if is_valid_case_number(case_number) == False:
-    print("Invalid Case Number!")
+def is_legacy_format(case_number):
+    if len(case_number) != 12:
+        return False
+    
+    if case_number[4] != "-":
+        return False
+
+    year = case_number[0:2]
+    county = case_number[2:4]
+    case_type = case_number[5:7]
+    case_number_part = case_number [7:]
+
+    if not year.isdigit():
+        return False
+
+    if not county.isdigit():
+        return False
+
+    if not case_type.isalpha() or not case_type.isupper():
+        return False
+
+    if not case_number_part.isdigit():
+        return False
+
+    return True
+
+
+def is_appeals_format(case_number):
+    if not (7 <= len(case_number) <= 10):
+        return False
+
+    district = case_number[0:2]
+    case_part = case_number[2:]
+
+    if not district.isalpha() or not district.isupper():
+        return False
+
+    if not case_part.isdigit():
+        return False
+
+    return True
+
+def is_valid_case_number(case_number):
+    return (
+        is_modern_format(case_number)
+        or is_legacy_format(case_number)
+        or is_appeals_format(case_number)
+    )
+
+if is_valid_case_number(case_number):
+    print("searching")
 else:
-    print("Searching...")
+    print("Invalid case number!") 
+   
